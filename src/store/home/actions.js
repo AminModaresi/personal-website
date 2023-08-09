4ac3a2aafdd6d04c4f8e7a7bb428d7356de07d7d
Amin Modaresi <78099755+AminModaresi@users.noreply.github.com>
2023-08-09 08:57:22 +0330

    Update actions.js

--- src/store/home/actions.js
+++ src/store/home/actions.js
@@ -3,7 +3,7 @@ export default {
         const response = await fetch("https://web-person-c896a-default-rtdb.firebaseio.com/quotation.json");
         const responseData = await response.json();
         let massage = ""
-        massage = responseData['-MznSCxm5YOuIoY0I6uu']
+        massage = responseData['-NSo9VSEnkzeQWpWYlZH']
         context.commit("getQuotation" , massage)
     },
     async postQuotation(_ , payload){
